ee6eab59066ed640ab04ecf63b9b187db0234e23
Peeeaje <yamajun88@me.com>
2024-04-21 13:42:04 +0900

    lessen screen flicker

--- scripts/zellij-switch-session.sh
+++ scripts/zellij-switch-session.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-zellij action start-or-reload-plugin "file:$HOME/.cargo/bin/zellij-session-switcher.wasm" -c "session_name=$1,dir=$2"
+zellij plugin -c "session_name=$1,dir=$2" -- "file:$HOME/.cargo/bin/zellij-session-switcher.wasm"
+exit 0
